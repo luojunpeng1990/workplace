308e81311fe0581c80a24c7a66b9d60bc28b44fa
longbo <925314862@qq.com>
2018-01-24 21:07:54 +0800

    234

--- CXSJ/c282/chapter16/234/234.c
+++ CXSJ/c282/chapter16/234/234.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(int argc  char *[] argv)
+int main(int argc, char ** argv )
 {
 	FILE *fp1,*fp2,*fp3;
-    char file[20],file[20],file3[20],ch;
+    char file1[20],file2[20],file3[20],ch;
     int file_number;
 	printf("Please input file1:");
 	scanf("File:\n");
-	if((fp1 = fopen(file1,"rb"))! = NULL)
+	if((fp1 = fopen(file1,"rb")) != NULL)
 	{
 		ch = fgetc(fp1);
 		while(ch != EOF)
@@ -24,12 +24,41 @@ int main(int argc  char *[] argv)
 	}
 	printf("\nPlease input file2:");
 	scanf("%s",file2);
-	printf("File2:\n);
-		if(
+	printf("File2:\n");
+		if((fp2 = fopen(file2,"rb")) != NULL)
+		{
+			ch = fgetc(fp2);
+			while(ch!=EOF)
+			{
+				putchar(ch);
+				ch = fgetc(fp2);
+			}
+		}
+		else
+		{
+			printf("Can not open!\n");
+			exit(1);
+		}
+		printf("\nPlease input file3:");
+		scanf("%s",file3);
+		printf("File3:\n");
+		if((fp3 = fopen(file3,"rb"))!=NULL)
+		{
+			ch = fgetc(fp3);
+			while(ch != EOF)
+			{
+				putchar(ch);
+				ch = fgetc(fp3);
+			}
+		}
+		else
+		{
+			printf("Can not open!\n");
+			exit(1);
+		}
 
+		file_number = fcloseall();
+		printf("\n%d Files closed\n",file_number);
+		return 0;
 
-
-
-
-	return 0;
 }
